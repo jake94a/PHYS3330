c0a514e12d37744250c5e75aa80e04de7bf641f3
FoundedSine14 <54042868+jake94a@users.noreply.github.com>
2020-01-09 16:40:05 -0700

    added comments

--- HW1.1.py
+++ HW1.1.py
@@ -1,3 +1,19 @@
+"""
+Description:
+Make a user-input function that takes a positive integer input n and returns n values of the fibonacci sequence
+Use tkinter to allow users to input their value via dialogue box
+User matplotlib to plot the returned values
+
+Function:
+root.withdraw() allows tkinter windows to open and close
+Ask the user for integer input using simpledialog
+The fibonacci sequence is: sum(x, x-1) starting at x=0 where (n-1) doesn't exist if x=0
+If the user inputs n=0, the function should just return 0
+Take the user input and add it to pre-defined 0 and 1
+The first few values of the fibonacci sequence are [0, 1, 1, 2, 3, 5, 8, 13...]
+"""
+
+
 import tkinter
 import matplotlib.pyplot as plt
 
