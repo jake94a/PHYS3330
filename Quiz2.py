b8f8f07b3df61b4b86e56210f88e7005e0e0ae2a
FoundedSine14 <54042868+jake94a@users.noreply.github.com>
2020-01-17 09:09:49 -0700

    Add files via upload
    Revised iteration to not throw an exception error. Added description

--- Quiz2.py
+++ Quiz2.py
@@ -1,15 +1,57 @@
+"""
+Description:
+Create a function that generates a list of random numbers then returns the max and min of that list
+
+Function:
+This function will use the 'random' and 'tkinter' python packages
+Display a dialog box to the user explaining what's about to happen so they can prepare themselves
+Ask the user to input an integer value for the size of list that they want "num values"
+Ask the user to input an integer value for the lower bound of random numbers to be generated
+Ask the user to input an integer value for the upper bound of random numbers to be generated
+Define an initial "min value" as the upper bound of random numbers
+    Do this to ensure that, if the list has more than one unique value, a new "min value" will be assigned later
+Define an initial "max value" as the lower bound of random numbers
+    Do this to ensure that, if the list has more than one unique value, a new "max value" will be assigned later
+Create a list of length "num values"
+Iterate through the list and assign a random integer to each index
+If the iterative value is greater or less than the current "max value" or "min value"
+    then reassign the respective variable to the iterative value
+    If the iterative value is not greater or less than the current "max value" or "min value" then don't do anything
+Return both max and min values to the user
+"""
+
 import random
 
-my_list = []
-for i in range(100):
-    my_list.append(random.randint(0, 100))
+from tkinter import messagebox
+from tkinter import simpledialog
 
-max_num = 0
-for i in range(len(my_list)):
-    try:
-        if my_list[i] > my_list[i + 1]:
-            max_num = my_list[i]
-    except IndexError:
-        pass
 
-print(max_num)
+def max_min():
+    messagebox.showinfo("Description", "This program will return the max and min values from a random set of numbers "
+                                       "generated with the requirements that you, the user, set.")
+    num_values = simpledialog.askinteger("Input", "Input how large of a list you would like")
+    lower_bound = simpledialog.askinteger("Input", "Input a lower bound of random values")
+    upper_bound = simpledialog.askinteger("Input", "Input an upper bound of random values")
+    my_list = []
+    max_val = lower_bound
+    min_val = upper_bound
+    test_value_string = f'True max is {max(my_list)}. True min is {min(my_list)}'
+
+    for i in range(num_values):
+        my_list.append(random.randint(lower_bound, upper_bound))
+
+    for i in range(len(my_list)):
+        if my_list[i] > max_val:
+            max_val = my_list[i]
+        elif my_list[i] < min_val:
+            min_val = my_list[i]
+        else:
+            pass
+
+    messagebox.showinfo("Output", f'The max value generated is {max_val}. The min value generated is {min_val}.')
+    print(my_list)
+    return max_val, min_val
+
+
+if __name__ == '__main__':
+    max_min()
