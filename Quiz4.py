25a6effaa54a9eeb1d325a643bcf77e662b22602
FoundedSine14 <54042868+jake94a@users.noreply.github.com>
2020-02-04 11:54:13 -0700

    Update Quiz4.py

--- Quiz4.py
+++ Quiz4.py
@@ -1,14 +1,27 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-x = 8
+a = 2
+x = np.arange(a, 5, 0.01)
 f = np.log(1 + x)
-n = 25
 
-ms = 0
-for i in range(1, n + 1):
-    print(ms, i)
-    ms += pow(-1, i + 1) * pow(x, i) / i
 
-print(f)
-print(ms)
+def log_taylor(n_terms):
+    error_list = []
+    ms = np.log(1 + a)
+    for i in range(1, n_terms + 1):
+        # print(ms, i)
+        nth_term = pow(-1, i + 1) * pow((x - a), i) / (i * pow((1 + a), i))
+        ms += nth_term
+        error_list.append(nth_term)
+    return ms
+    # plt.title(f'Error is {nth_term[-1]}')
+
+log_taylor(5)
+plt.plot(x, f)
+plt.plot(x, log_taylor(3))
+plt.plot(x, log_taylor(5))
+plt.plot(x, log_taylor(10))
+plt.plot(x, log_taylor(20))
+plt.figlegend(("Original Plot", 'n = 3', 'n = 5', 'n = 10', 'n = 20'))
+plt.show()
