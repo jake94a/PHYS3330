1c30a132af910725f97cac891c5e3a3df0c90bee
FoundedSine14 <54042868+jake94a@users.noreply.github.com>
2020-02-03 20:47:39 -0700

    Add files via upload

--- all_the_things.py
+++ all_the_things.py
@@ -11,6 +11,10 @@ def sin_cos():
     return my_func
 
 
+"""
+x_num_list is 629 values
+I need fnc_sum to also be a list of 629 values, for each x_num value
+"""
 def sin_cos_taylor(n_terms):
     x_num = numpy.arange(0, 2 * numpy.pi, 0.01)
     x_num_list = list(x_num)
@@ -18,11 +22,13 @@ def sin_cos_taylor(n_terms):
     for i in range(n_terms + 1):
         for j in range(len(x_num_list)):
             return_list.append(((-1) ** i) * (
-                (3 * (x_num_list[j] ** (2 * i)) / math.factorial(2 * i)) +
-                (2 * (x_num_list[j] ** ((2 * i) + 1)) / math.factorial((2 * i) + 1))))
+                    (3 * (x_num_list[j] ** (2 * i)) / math.factorial(2 * i)) +
+                    (2 * (x_num_list[j] ** ((2 * i) + 1)) / math.factorial((2 * i) + 1))))
+    fnc_sum = sum(return_list)
     plt.title("hw4.3")
-    plt.plot(x_num, return_list)
+    plt.plot(x_num, fnc_sum)
     return return_list
+
 
 plt.plot(sin_cos_taylor(50))
 plt.show()
